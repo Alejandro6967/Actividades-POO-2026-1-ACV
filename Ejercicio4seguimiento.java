a063f263c15782076b125f855e8d7582c313c279
Alejandro6967 <acastillav@unal.edu.co>
2026-05-10 18:37:02 -0500

    seguimiento 1 ejercicio #4

---
+++ Ejercicio4seguimiento.java
@@ -0,0 +1,28 @@
+package com.mycompany.actividadespoo.acv; //encontrar el archivo
+import java.util.Scanner; // Importamos para pedir datos
+
+public class Ejercicio4seguimiento { //este es el programa general
+    
+    public static void main (String[] args) {
+        
+        //inicio
+        int edadjuan, edadalberto, edadana, edadmama; //Crea cajas para # enteros, vacías para agregarles datos
+        
+        Scanner entrada_teclado = new Scanner(System.in); //para recibir cosas del teclado
+        System.out.println("Ingrese la edad de Juan:"); // pedimos al usuario la edad juan
+        edadjuan = entrada_teclado.nextInt(); // espera a que escribas el número y des Enter
+        
+        
+        
+        //trae a juan y opera y usa la caja vacía para guardar el resultado
+        edadalberto = (2*edadjuan)/3; 
+        edadana=(4*edadjuan)/3; //Se guarda en la caja de edadana.
+        edadmama= edadjuan + edadana + edadalberto;
+        
+        System.out.println("La edad de Juan es: " + edadjuan); //El signo de (+) aquí no está está pegando (concatenandolos)
+        System.out.println("La edad de Alberto es: " + edadalberto);
+        System.out.println("La edad de Ana es: " + edadana);
+        System.out.println("La edad de la Mama es: " + edadmama);
+    }
+}
+//cierra
