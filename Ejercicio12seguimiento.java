08de3b9820d690d922275523e7423d729d63244c
Alejandro6967 <acastillav@unal.edu.co>
2026-05-10 22:23:31 -0500

    Ejercicio12Seguimiento

---
+++ Ejercicio12seguimiento.java
@@ -0,0 +1,34 @@
+package com.mycompany.actividadespoo.acv;
+public class Ejercicio12seguimiento {
+    
+    //inician los metodos
+    public static double calcular_Completo(double Horas, double Salario){
+        double Completo = Horas * Salario;
+        return Completo;
+    }
+    
+    public static double calcular_Retencion(double Completo, double Retencionporcentaje){
+        double Retencion = Completo * Retencionporcentaje;
+        return Retencion;
+    }
+
+    public static double calcular_Neto(double Completo, double Retencion){
+        double Neto = Completo - Retencion;
+        return Neto; //Se guarda 
+    }
+    
+    public static void main(String[] args) {
+       double Completo, Horas, Neto, Retencionporcentaje, Salario, Retencion;    
+       Horas=48;
+       Salario=5000;
+       Retencionporcentaje=(12.5/100);
+       
+        Completo = calcular_Completo(Horas, Salario);
+        Retencion = calcular_Retencion(Completo, Retencionporcentaje);
+        Neto = calcular_Neto(Completo, Retencion);
+
+        System.out.println("El salario completo es: " + Completo);
+        System.out.println("La Retencion es: " + Retencion);
+        System.out.println("El salario Neto es: " + Neto); 
+    }
+}
